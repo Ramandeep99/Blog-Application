26d45c321ea70dfba167512f2759087225e8d3bd
Raman <kambojji785@gmail.com>
2021-08-26 22:46:42 +0530

    partial setup of app and login signup done

--- routes/articles.js
+++ routes/articles.js
@@ -1,36 +1,70 @@
 const express = require('express');
-// const article = require('./../models/article');
 const Article = require('./../models/article')
 const router = express.Router();
+const path = require('path');
+const app = express();
 
-router.get('/new', (req, res) => {
-    res.render("articles/new", { article: new Article() })
-})
 
-router.get('/:id', async (req, res) => {
-    const article = await Article.findById(req.params.id)
-    if (article == undefined) {
-        res.redirect('/')
-    }
-    else {
-        res.render('articles/show', { article: article })
-    }
-})
+const template_path = path.join(__dirname,'../templates/views');
+console.log(template_path);
+console.log(999);
+// console.log(router)
+app.set('views' , template_path)
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    console.log(article);
-    try {
-        article = await article.save()
-        res.redirect(`/articles/${article.id}`)
-    }
-    catch (error) {
-        res.render('articles/new', { article: article })
-    }
-})
+// router.get('/new', (req, res) => {
+//     res.send('raman')
+// })
 
-module.exports = router
+// router.get('/new', (req, res) => {
+//     res.render("template_path/new", { article: new Article() })
+// })
+
+// router.get('/edit/:id', async (req, res) => {   
+//     const article = await Article.findById(req.params.id)
+//     res.render("articles/edit", { article: article })
+// })
+
+// router.get('/:slug', async (req, res) => {
+//     const article = await Article.findOne({ slug: req.params.slug })
+//     if (article == undefined) {
+//         res.redirect('/')
+//     }
+//     else {
+//         res.render('articles/show', { article: article })
+//     }
+// })
+
+// router.post('/', async (req, res, next) => {
+//     req.article =  new Article()
+//     next()
+// }, PostAndPut('new'))
+
+// router.put('/:id', async (req, res, next) => {
+//     req.article = await Article.findById(req.params.id)
+//     next()
+// }, PostAndPut('edit'))  
+
+
+// function PostAndPut(path) {
+//     return async (req, res) => {
+//         let article = req.article
+//         article.title = req.body.title
+//         article.description = req.body.description
+//         article.markdown = req.body.markdown
+
+//         try {
+//             article = await article.save()
+//             res.redirect(`/articles/${article.slug}`)
+//         }
+//         catch (error) {
+//             res.render(`articles/${path}`, { article: article })
+//         }
+//     }
+// }
+
+// router.delete('/:id', async (req, res) => {
+//     await Article.findByIdAndDelete(req.params.id)
+//     res.redirect('/')
+// })
+
+// module.exports = router
